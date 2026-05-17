8d8a7c5b4009dd8ab1c36146a4e840f8177f6cdb
maksi <max.permyakov100@gmail.com>
2026-05-17 18:18:43 +0300

    Add project files.

---
+++ Models/TaskItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskTracker.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; }
+
+
+        [MaxLength(1000)]
+        public string? Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        [Display(Name="Created")]
+        public DateTime CreatedAt { get; set; }= DateTime.Now;
+    }
+}
